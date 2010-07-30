ae6339b2219f0d15f05e79919eed2a9ab3a21ef5
Boris Jakubith <runkharr@googlemail.com>
2010-07-30 06:43:15 +0000

    - dcedit: using the version from `suex´ (it contains the `debian/control´-file     parser), but extended this version with a new mode: 	`dcedit -m´     writes the maintainer to stdout (either from the file MAINTAINER in the     top-directory of the source-tree or from the file `debian/control´     (either below the top-directory or below the admin sub-directory) or from     the last (top-most) section of `debian/changelog´ (in the same directory     as `debian/control´) ... - gendeb.sh: many fixes; uses `dcedit -m´ for detecting the right maintainer;     far from ready now - as there are general changes in the concept to be     done ...

--- gendeb.sh
+++ gendeb.sh
@@ -7,14 +7,35 @@
 # Copyright: © 2005, Boris Jakubith <bj@isv-gmbh.de>
 # Licence: GPL(v2)
 
+PROG="${0##*/}"
+fp="${0%/*}/fullpath"
+top="`$fp -b 2 "$0"`"
+adm="$top/admin"
+
+error() {
+    local ec="$1"; shift
+    echo 1>&2 "${PROG}:" "$@"
+    exit $ec
+}
+
 # Get the last version number of the `changelog' file ...
 #
-DV=`head -1 admin/debian/changelog|\
+if [ -d "$top/debian" ]; then
+    deb_source="$top/debian"
+    deb_changelog="$top/debian/changelog"
+else
+    deb_source="$adm/debian"
+    deb_changelog="$adm/debian/changelog"
+fi
+
+test -f "$deb_changelog" || error 66 "'$deb_changelog' not found"
+
+DV=`head -1 "$deb_changelog"|\
     sed -e 's|[^(]*(\([0-9]\+\.[0-9.]\+\)[^)]*).*|\1|'`
 #
 # and the current package version ...
 #
-PV=`cat VERSION`
+PV="`cat "$top/VERSION"`"
 
 # Check if both version numbers differ ...
 #
@@ -22,27 +43,29 @@ if [ "$DV" != "$PV" ]; then
     # If so, then insert a new entry into the `debian/changelog'-directory ...
     #
     # ... generate a new debian version number ...
-    DV=`head -1 admin/debian/changelog|\
+    DV=`head -1 "$deb_changelog"|\
 	sed -e "s|.*(\([0-9]\+\.[0-9]\+\)\(.*\)).*|$PV\2|"`
 
     # ... generate the date for the `AUTHOR  DATE'-entry ...
     DT=`unset LANG LC_CTYPE LC_ALL; date -R`
 
+    package="`$top/admin/version packagename`"
+    maintainer="`$top/admin/dcedit -m`"
     # ... generate the new entry ...
-    cat <<-EOT >admin/debian/changelog.new
-	rcheckrcpt ($DV) unstable; urgency=low
+    cat <<-EOT >"${deb_changelog}.new"
+	$package ($DV) unstable; urgency=low
 
 	  * New upstream release.
 	
-	 -- Boris Jakubith <bj@isv-gmbh.de>  $DT
+	 -- $maintainer  $DT
 	
 	EOT
     # ... append the existing `debian/changelog'-file ...
-    cat admin/debian/changelog >>admin/debian/changelog.new
-    # ... copy back to `debian/changelog' ...
-    cat admin/debian/changelog.new >admin/debian/changelog
+    cat "$deb_changelog" >>"${deb_changelog}.new"
+    # ... copy back to `debian/changelog´ ...
+    cat "${deb_changelog}.new" >"$deb_changelog"
     # ... remove the temporary file ...
-    rm -f admin/debian/changelog.new
+    rm -f "${deb_changelog}.new"
 fi
 
 if [ $# -gt 0 -a "$1" = changelog ]; then exit 0; fi
@@ -53,29 +76,58 @@ mkdir -m 0755 'debdir';
 
 # Copy the required files into it ...
 #
-find . -name 'CVS' -prune -o -path '*/CVS/*' -prune -o -name 'debdir' -prune \
+find . \( -name 'CVS' -o -path '*/CVS/*' \) -prune \
+       -o \( -name '.svn' -o -path '*/.svn/*' \) -prune \
+       -o -name 'debdir' -prune \
        -o -path 'debdir/*' -prune -o -print \
-| cpio -pdvm debdir
+| cpio -pdm debdir
 
-# Change into the `Debian' source directory ...
+# Remove the debian sub-directory in the target, because it will be replaced
+# by another one ...
 #
-cd 'debdir'
+rm -rf debdir/debian
 
-# Move the `debian' sub-directory from `admin/' to here ...
+# Copy the `debian' sub-directory from eithet $top or $adm to here ...
 #
-cp -a ../admin/debian .
-../admin/dcedit
+if [ -d "$top/debian" ]; then
+    cpsrc="$top/debian"; d="$top"
+else
+    cpsrc="$adm/debian"; d="$adm"
+fi
 
-# Use `dpkg-buildpackage' in order to generate the debian packages ...
+# Change into the "debian" source directory and copy all files there (with the
+# exception of the `CVS´ and `.svn´ sub-directories) to the `debdir´ sub-
+# directory in the previous directory ...
+#
+oldwd="`pwd`"; cd "$d"
+find debian \( -name CVS -o -path '*/CVS/*' \) -prune \
+	    -o \( -name .svn -o -path '*/.svn/*' \) -prune \
+	    -o -print \
+| perl -ne "s|^\\Q$d\\E/|./|; "'print $_;' \
+| cpio -pdm "$oldwd/debdir"
+exit
+
+# Change back into $oldwd, but go a step further by changing into the sub-
+# directory `debdir´ below $oldwd, because this is out build-directory ...
+#
+cd "$oldwd/debdir"
+
+# Now modify (extend) the file `debian/changelog´ below the build-directory ...
+#
+"$adm"/dcedit
+
+# Use `dpkg-buildpackage' for generating the debian packages, but only the
+# binary packages and without signing these packages ...
 #
 dpkg-buildpackage -rfakeroot -b -us -uc
 
-# Back to parent directory and remove the `Debian' source directory ...
+# Change back into parent directory and remove the build-directory
+# (`debdir´) ...
 #
 cd ..; rm -rf debdir
 
 # Last not least, move the `.deb'-files outside of this directory ...
 #
-mv *.deb *.changes ..
+#mv *.deb *.changes ..
 
 exit 0
