86fd68330a53c1dff43168795529e84056f0c7a9
Boris Jakubith <runkharr@googlemail.com>
2013-03-02 13:58:02 +0000

    - sh-wrapper: added `-I$(PPATH)´ to the `cc´-invocation - allowing for the use     of the new function (source-)library ... - run.c: moved `cwd()´ and `which()´ into the new (source-)library ...

--- run.c
+++ run.c
@@ -52,65 +52,9 @@ static void check_ptr (const char *where, void *ptr)
     ifnull (ptr) { report (EX_OSERR, errno, "(in %s)", where); }
 }
 
-static const char *cwd (void)
-{
-    static size_t wdsz = 0;
-    static char *wd = NULL;
-    if (wdsz == 0) {
-	wdsz = 1024;
-	check_ptr ("cwd", wd = t_allocv (char, wdsz));
-    }
-    for (;;) {
-	unlessnull (getcwd (wd, wdsz)) { return wd; }
-	wdsz += 1024;
-	check_ptr ("cwd", wd = realloc (wd, wdsz));
-    }
-}
+#include "lib/cwd.c"
 
-static const char *which (const char *cmd)
-{
-    static char *rv = NULL;
-    static size_t rvsz = 0;
-    static char *PATH = NULL;
-    char *p, *q;
-    size_t pl, cmdsz = strlen (cmd);
-    struct stat sb;
-    unlessnull (strchr (cmd, '/')) { return cmd; }
-    ifnull (PATH) {
-	PATH = getenv ("PATH");
-	ifnull (PATH) {
-	    if (geteuid () == 0) {
-		PATH = "/root/bin:/usr/bin:/usr/sbin:/bin:/sbin:"
-		       "/usr/local/sbin";
-	    } else {
-		p = (char *) cwd ();
-		q = t_allocv (char, strlen (p) + strlen ("/bin") + 1);
-		check_ptr ("which", q);
-		sprintf (q, "%s/bin", p);
-		p = "/usr/local/bin:/usr/bin:/bin";
-		PATH = t_allocv (char, strlen (q) + strlen (p) + 2);
-		check_ptr ("which", PATH);
-		sprintf (PATH, "%s:%s", q, p);
-		free (q);
-	    }
-	}
-    }
-    p = PATH;
-    while (*p != '\0') {
-	q = strchr (p, ':'); ifnull (q) { q = &p[strlen (p)]; }
-	if (p != q) {
-	    pl = (q - p) + cmdsz + 2;
-	    if (pl > rvsz) { check_ptr ("which", rv = realloc (rv, pl)); }
-	    sprintf (rv, "%.*s/%s", (int) (q - p), p, cmd);
-	    if (stat (rv, &sb)) { goto NEXT; }
-	    if (! S_ISREG(sb.st_mode)) { goto NEXT; }
-	    if (access (rv, X_OK) == 0) { return rv; }
-	}
-NEXT:
-	p = q; if (*p == ':') { ++p; }
-    }
-    return NULL;
-}
+#include "lib/which1.c"
 
 int main (int argc, char *argv[])
 {
