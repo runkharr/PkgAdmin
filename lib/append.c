f2fa30a9cd50ca2d83a55b50136752868a77008b
Boris Jakubith <runkharr@googlemail.com>
2013-03-03 02:39:07 +0000

    - lib/append.c: fixed some BUGs in `_lappend()´ and added comments describing     this function for me ...

--- lib/append.c
+++ lib/append.c
@@ -49,21 +49,42 @@ char *_lappend (char **_res, size_t *_ressz, char *p, const char *s, size_t l)
     char *res = *_res;
     size_t ressz = *_ressz, px = 0;
     if (res) {
+	/* p == NULL means: start at the beginning of `res´ ... */
+	if (!p) { p = res; }
+	/* p must not point outsize of res ... res+ressz-1 */
 	if (p < res || p >= res + ressz) { errno = EINVAL; return NULL; }
+	/* Save `p´ as "index" px ... */
 	px = (size_t) (p - res);
 	if (ressz - px - 1 < l) {
+	    /* Not enough space ==> increase the space ... */
 	    ressz += l - (ressz - px - 1) + 128; ressz -= (ressz % 128);
 	    if (!(res = realloc (*_res, ressz))) {
 		free (*_res); *_res = NULL; *_ressz = 0; return NULL;
 	    }
+	    /* Put the changed buffer-parameters back into `*_res´ and
+	    ** `*_ressz´ ...
+	    */
 	    *_res = res; *_ressz = ressz;
 	}
     } else {
+	/* If there is no buffer yet, p must not point to anything ... */
 	if (p) { errno = EINVAL; return NULL; }
+	/* calculate a memory size which is rounded up to the next size which
+	** is a multiple of 128 ...
+	*/
 	ressz = l + 128; ressz -= (ressz % 128);
+	/* Allocate the buffer ... */
 	if (!(res = malloc (ressz))) { return NULL; }
+	/* Put the changed buffer-parameters back into `*_res´ and
+	** `*_ressz´ ...
+	*/
+	*_res = res; *_ressz = ressz;
     }
+    /* Set `p´ back to the (corresponding) position in the (new) buffer and
+    ** copy the string to this position ...
+    */
     p = res + px; memcpy (p, s, l); p[l] = '\0';
+    /* Return the position directly behind the increased string ... */
     return p + l;
 }
 
