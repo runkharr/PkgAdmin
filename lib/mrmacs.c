b5675c95b59367b6b8af0bb932f21708fb1c237b
Boris Jakubith <runkharr@googlemail.com>
2013-03-03 20:47:13 +0000

    - lib/mrmacs.c: now using my own versions of `malloc()´ and `realloc()´ -     names `mallocmb()´ and `reallocmb()´ which set the requested memory size     to a minimum value of 64 bytes (it seems that blocks with less than 64     bytes in size cause some problems with some newer version of the Glibc) ...

--- lib/mrmacs.c
+++ lib/mrmacs.c
@@ -14,18 +14,32 @@
 #ifndef MRMACS_C
 #define MRMACS_C
 
+#include "lib/gnu-inline.c"
+
 #define ifnull(x) if ((void *)(x) == NULL)
 #define unlessnull(x) if ((void *)(x) != NULL)
 #define isNULL(x) ((void *)(x) == NULL)
 #define noNULL(x) ((void *)(x) != NULL)
-#define t_alloc(t) ((t *) malloc (sizeof (t)))
-#define t_allocv(t, n) ((t *) malloc ((n) * sizeof (t)))
-#define t_allocp(t, n) ((t *) malloc (sizeof (t) + (n)))
-#define t_realloc(t, p, n) ((t *) realloc ((p), (n) * sizeof(t)))
+#define t_alloc(t) ((t *) mallocmb (sizeof (t)))
+#define t_allocv(t, n) ((t *) mallocmb ((n) * sizeof (t)))
+#define t_allocp(t, n) ((t *) mallocmb (sizeof (t) + (n)))
+#define t_realloc(t, p, n) ((t *) reallocmb ((p), (n) * sizeof(t)))
 #define cfree(p) do { \
     void **q = (void **) &(p); \
     if (*q) { free (*q); *q = 0; } \
 } while (0)
+
+static __inline__ void *mallocmb (size_t sz)
+{
+    if (sz < 64) { sz = 64; }
+    return malloc (sz);
+}
+
+static __inline__ void *reallocmb (void *p, size_t sz)
+{
+    if (sz < 64) { sz = 64; }
+    return realloc (p, sz);
+}
 
 #endif /*MRMACS_C*/
 
