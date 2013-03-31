f30db4fe47aa5dc476d6fa1d683fcd8e1e23b9c5
Boris Jakubith <runkharr@googlemail.com>
2013-03-31 15:41:55 +0000

    - admin/fullpath.c: added an option '-p' (search for the given argument in the     PATH and then use it's absolute pathname); moved 'trans_path()' (including     all it's constants and tables) to a new file 'lib/trans_path.c' and     inserted an '#include "lib/trans_path.c"' instead; the reason behind that     is that i'm now using 'trans_path()' in a second 'admin/'-program     ('admin/genflist.c') ...

--- fullpath.c
+++ fullpath.c
@@ -30,6 +30,8 @@
 #include "lib/bconc.c"
 /*#include "lib/sdup.c"*/
 #include "lib/puteol.c"
+#include "lib/bwhich.c"
+#include "lib/trans_path.c"
 
 static void usage (const char *format, ...)
 {
@@ -46,98 +48,18 @@ static void usage (const char *format, ...)
     exit (0);
 }
 
-#define CCEOS 0
-#define CCSLASH 1
-#define CCDOT 2
-#define CCOTHER 3
-
-#define S0 0
-#define S1 1
-#define S2 2
-#define S3 3
-#define S4 4
-#define S5 5
-#define S6 6
-
-static const char strans[7][4] = {
-    /*S0*/ { S6, S1, S6, S6 },
-    /*S1*/ { S5, S1, S2, S4 },
-    /*S2*/ { S5, S1, S3, S4 },
-    /*S3*/ { S5, S4, S4, S4 },
-    /*S4*/ { S5, S1, S4, S4 },
-    /*S5*/ { -1, -1, -1, -1 },
-    /*S6*/ { -2, -2, -2, -2 }
-};
-
-#define NOP 0
-#define STORE 1
-#define SKIP 2
-#define DOTSTORE 3
-#define BACK 4
-#define DOTDOTSTORE 5
-#define TERMINATE 6
-#define ERROR 7
-
-static const char saction[7][4] = {
-    /*S0*/ { ERROR, STORE, ERROR, ERROR },
-    /*S1*/ { TERMINATE, SKIP, SKIP, STORE },
-    /*S2*/ { TERMINATE, SKIP, SKIP, DOTSTORE },
-    /*S3*/ { BACK, BACK, DOTDOTSTORE, DOTDOTSTORE },
-    /*S4*/ { TERMINATE, STORE, STORE, STORE },
-    /*S5*/ { TERMINATE, ERROR, ERROR, ERROR },
-    /*S6*/ { ERROR, ERROR, ERROR, ERROR }
-};
-
-static int trans_path (char *p, char *q)
-{
-    int cclass, action, state = S0, lastch = 0;
-    char *sp = p;
-    for (;;) {
-	switch (*q) {
-	    case '\0': cclass = CCEOS; break;
-	    case '/':  cclass = CCSLASH; break;
-	    case '.':  cclass = CCDOT; break;
-	    default:   cclass = CCOTHER; break;
-	}
-	action = saction[state][cclass];
-	state = strans[state][cclass];
-	switch (action) {
-	    case NOP:
-		lastch = *q; break;
-	    case STORE:
-		lastch = *q; *p++ = *q++; break;
-	    case SKIP:
-		lastch = *q; ++q; break;
-	    case DOTSTORE:
-		lastch = *q; *p++ = '.'; *p++ = *q++; break;
-	    case BACK:
-		--p; if (*p == '/' && p > sp) { --p; }
-		while (p > sp && *--p != '/');
-		lastch = *q;
-		break;
-	    case DOTDOTSTORE:
-		lastch = *q; *p++ = '.'; *p++ = '.'; *p++ = *q++; break;
-	    case TERMINATE:
-		if (lastch == '/' && p > sp) { --p; }
-		*p = '\0'; return 0;
-	    case ERROR:
-		return -1;
-	}
-    }
-}
-
 #define MAXLEVEL 1024
 
 int main (int argc, char *argv[])
 {
-    int opt, level = 0, ix, errcc = 0, newitem, dots, lastch;
+    int opt, level = 0, ix, errcc = 0, newitem, dots, lastch, use_path = 0;
     unsigned long v;
     const char *wd;
     char *p, *q, *buf = NULL, *ep, *sp, *path;
     size_t bufsz = 0;
 
     set_prog (argc, argv);
-    while ((opt = getopt (argc, argv, ":b:h")) != -1) {
+    while ((opt = getopt (argc, argv, ":b:hp")) != -1) {
 	switch (opt) {
 	    case 'b':
 		v = strtoul (optarg, &p, 10);
@@ -148,6 +70,7 @@ int main (int argc, char *argv[])
 		level = (int) v;
 		break;
 	    case 'h': usage (NULL); break;
+	    case 'p': use_path = 1; break;
 	    case ':':
 		usage ("missing argument for option '-%c'", optopt);
 		break;
@@ -163,6 +86,8 @@ int main (int argc, char *argv[])
     for (ix = optind; ix < argc; ++ix) {
 	if (*(path = argv[ix]) == '/') {
 	    p = bconc (buf, bufsz, path);
+	} else if (use_path && !strchr (path, '/')) {
+	    p = bwhich (buf, bufsz, path);
 	} else {
 	    p = bconc (buf, bufsz, wd, "/", path);
 	}
