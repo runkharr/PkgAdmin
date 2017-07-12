c15bd4e3741a8877b50ed0122e0ef7d4a88bd440
Boris Jakubith <runkharr@googlemail.com>
2017-07-12 10:17:31 +0000

    - lib/trans_path.c: Rewrote 'trans_path()':     1. First, no constants specific to this function are any longer visible        outside of it.     2. It needs only five states (instead of the seven the previous version        required) and five actions.     3. It catches the case that the pathname is not absolute (not starting        with a '/') and returns -1 in this case. In each other case, 0 is        returned.     4. It has been tested (even with longer '../'-chains).

--- lib/trans_path.c
+++ lib/trans_path.c
@@ -14,87 +14,57 @@
 #ifndef TRANS_PATH_C
 #define TRANS_PATH_C
 
-#define CCEOS 0
-#define CCSLASH 1
-#define CCDOT 2
-#define CCOTHER 3
-
-#define S0 0
-#define S1 1
-#define S2 2
-#define S3 3
-#define S4 4
-#define S5 5
-#define S6 6
-
-static const char strans[7][4] = {
-	   /*CCEOS    CCSLASH  CCDOT    CCOTHER*/
-    /*S0*/ { S6,      S1,      S6,      S6 },
-    /*S1*/ { S5,      S1,      S2,      S4 },
-    /*S2*/ { S5,      S1,      S3,      S4 },
-    /*S3*/ { S5,      S4,      S4,      S4 },
-    /*S4*/ { S5,      S1,      S4,      S4 },
-    /*S5*/ { -1,      -1,      -1,      -1 },
-    /*S6*/ { -2,      -2,      -2,      -2 }
-};
-
-#define NOP 0
-#define STORE 1
-#define SKIP 2
-#define DOTSTORE 3
-#define BACK 4
-#define DOTDOTSTORE 5
-#define TERMINATE 6
-#define ERROR 7
-
-static const char saction[7][4] = {
-	  /* CCEOS      CCSLASH  CCDOT        CCOTHER*/
-    /*S0*/ { ERROR,     STORE,   ERROR,       ERROR },
-    /*S1*/ { TERMINATE, SKIP,    SKIP,        STORE },
-    /*S2*/ { TERMINATE, SKIP,    SKIP,        DOTSTORE },
-    /*S3*/ { BACK,      BACK,    DOTDOTSTORE, DOTDOTSTORE },
-    /*S4*/ { TERMINATE, STORE,   STORE,       STORE },
-    /*S5*/ { TERMINATE, ERROR,   ERROR,       ERROR },
-    /*S6*/ { ERROR,     ERROR,   ERROR,       ERROR }
-};
-
-static
-int trans_path (char *p, char *q)
+static int trans_path (char *p, const char *q)
 {
-    int cclass, action, state = S0, lastch = 0;
+    enum cclass { CCEOS = 0, CCSLASH, CCDOT, CCOTHER } cclass;
+    enum state { S0 = 0, S1, S2, S3, END }
+	state = S0, prev_state;
+    enum action { NOOP = 0, STORE, BACK1, BACK, STOP } action;
+    static const enum state strans[5][4] = {
+	       /* CCEOS    CCSLASH  CCDOT    CCOTHER*/
+	/*S0*/  { END,     S1,      S2,      S0 },
+	/*S1*/  { END,     S1,      S2,      S0 },
+	/*S2*/  { END,     S1,      S3,      S0 },
+	/*S3*/  { END,     S1,      S0,      S0 },
+	/*END*/ { END,     END,     END,     END },
+    };
+    static const enum action saction[5][4] = {
+	      /*  CCEOS      CCSLASH  CCDOT        CCOTHER*/
+	/*S0*/  { NOOP,      STORE,   STORE,       STORE },
+	/*S1*/  { NOOP,      NOOP,    STORE,       STORE },
+	/*S2*/  { BACK1,     BACK1,   STORE,       STORE },
+	/*S3*/  { BACK,      BACK,    STORE,       STORE },
+	/*END*/ { STOP,      STOP,    STOP,        STOP },
+    };
     char *sp = p;
+    if (*q != '/') { return -1; }
     for (;;) {
-	switch (*q) {
+	int ch = *q;
+	if (ch) { ++q; }
+	switch (ch) {
 	    case '\0': cclass = CCEOS; break;
 	    case '/':  cclass = CCSLASH; break;
 	    case '.':  cclass = CCDOT; break;
 	    default:   cclass = CCOTHER; break;
 	}
-	action = saction[state][cclass];
-	state = strans[state][cclass];
+	action = saction[(int) state][(int) cclass];
+	prev_state = state; state = strans[(int) prev_state][(int) cclass];
 	switch (action) {
-	    case NOP:
-		lastch = *q; break;
-	    case STORE:
-		lastch = *q; *p++ = *q++; break;
-	    case SKIP:
-		/* When skipping, lastch must remain the same */
-		/*lastch = *q;*/
-		++q; break;
-	    case DOTSTORE:
-		lastch = *q; *p++ = '.'; *p++ = *q++; break;
-	    case BACK:
-		--p; if (*p == '/' && p > sp) { --p; }
-		while (p > sp && *--p != '/');
-		lastch = *q;
+	    case NOOP:
 		break;
-	    case DOTDOTSTORE:
-		lastch = *q; *p++ = '.'; *p++ = '.'; *p++ = *q++; break;
-	    case TERMINATE:
-		if (lastch == '/' && p > sp) { --p; }
+	    case STORE:
+		*p++ = ch; break;
+	    case BACK1:
+		--p; break;
+	    case BACK:
+		/* The two dots (after the '/') were already stored, so we
+		** need to go back three steps ... */
+		p -= 3;
+		while (p > sp && *--p != '/');
+		++p;
+		break;
+	    case STOP:
 		*p = '\0'; return 0;
-	    case ERROR:
-		return -1;
 	}
     }
 }
