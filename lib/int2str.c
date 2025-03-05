90ccc730bd203f25891ecea07cdfe7eab2cd3577
Boris Jakubith <runkharr@googlemail.com>
2025-03-05 14:41:21 +0000

    - lib/int2str.c: (NEW)   * A very simple `int` to string conversion, but with buffer overflow     checking. The minimum `int` value (the one whjere where the sign bit     is set and all other bits are cleared) is currently not supported.

---
+++ lib/int2str.c
@@ -0,0 +1,59 @@
+/* int2str.c
+**
+** $Id$
+**
+** Author: Boris Jakubith
+** E-Mail: runkharr@googlemail.com
+** Copyright: (c) 2025, Boris Jakubith <runkharr@googlemail.com>
+** License: GNU General Public License, version 2
+**
+** A simple integer to string conversion into a given buffer.
+** if the integer fits into the buffer, a pointer to the buffer is
+** returned. Otherwise, `NULL` is returned and `errno` is set to `ENOBUFS`.
+** If the buffer pointer is `NULL` of the supplied buffer size is < 3, then
+** (again) `NULL` is returned and `errno` is set to `EINVAL`
+**
+*/
+#ifndef INT2STR_C
+#define INT2STR_C
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <errno.h>
+
+static const char *int2str (int val, char *buf, size_t bufsz, bool rightbound)
+{
+    bool sign = (val < 0);
+    size_t minsz = 2 + (sign ? 1 : 0);
+    char *res = 0;
+    if (! buf || bufsz < minsz) {
+	errno = EINVAL;
+    } else {
+	static const char digits[] = "0123456789";
+	int dg; bool buf_ovfl = false;
+	char *p = buf + bufsz;
+	*--p = '\0';
+	if (sign) { val = -val; }
+	do {
+	    dg = val % 10; val /= 10;
+	    if (! buf_ovfl) {
+		if (p > buf) { *--p = digits[dg]; } else { buf_ovfl = true; }
+	    }
+	} while (val > 0);
+	if (sign) {
+	    if (p > buf) { *--p = '-'; } else { buf_ovfl = true; }
+	}
+	if (p > buf) {
+	    if (rightbound) {
+		while (p > buf) { *--p = ' '; }
+	    } else {
+		char *q = buf; --q; --p;
+		while ((*++q = *++p));
+	    }
+	}
+	if (buf_ovfl) { errno = ENOBUFS; } else { res = buf; }
+    }
+    return res;
+}
+
+#endif /*INT2STR_C*/
